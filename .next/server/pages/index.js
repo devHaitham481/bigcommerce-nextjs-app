46ef53758974971aa468db3012690e60e198e194
devHaitham481 <devHaitham481@gmail.com>
2022-08-09 12:13:25 +0200

    initial nextjs app setup

---
+++ .next/server/pages/index.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @bigcommerce/big-design */ \"@bigcommerce/big-design\");\n/* harmony import */ var _bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst Index = ()=>/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_1__.Panel, {\n        header: \"Homepage\",\n        margin: \"xxLarge\",\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_bigcommerce_big_design__WEBPACK_IMPORTED_MODULE_1__.Text, {\n            children: \"Hello world\"\n        }, void 0, false, {\n            fileName: \"/home/exodusreed/Desktop/vertrical/nextjs-bigcommerce/pages/index.tsx\",\n            lineNumber: 5,\n            columnNumber: 5\n        }, undefined)\n    }, void 0, false, {\n        fileName: \"/home/exodusreed/Desktop/vertrical/nextjs-bigcommerce/pages/index.tsx\",\n        lineNumber: 4,\n        columnNumber: 3\n    }, undefined);\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Index);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQTtBQUFzRDtBQUV0RCxNQUFNRSxLQUFLLEdBQUcsa0JBQ1osOERBQUNGLDBEQUFLO1FBQUNHLE1BQU0sRUFBQyxVQUFVO1FBQUNDLE1BQU0sRUFBQyxTQUFTO2tCQUN2Qyw0RUFBQ0gseURBQUk7c0JBQUMsYUFBVzs7Ozs7cUJBQU87Ozs7O2lCQUNsQjtBQUdWLGlFQUFlQyxLQUFLLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9uZXh0anMtYmlnY29tbWVyY2UvLi9wYWdlcy9pbmRleC50c3g/MDdmZiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBQYW5lbCwgVGV4dCB9IGZyb20gJ0BiaWdjb21tZXJjZS9iaWctZGVzaWduJztcblxuY29uc3QgSW5kZXggPSAoKSA9PiAoXG4gIDxQYW5lbCBoZWFkZXI9XCJIb21lcGFnZVwiIG1hcmdpbj1cInh4TGFyZ2VcIj5cbiAgICA8VGV4dD5IZWxsbyB3b3JsZDwvVGV4dD5cbiAgPC9QYW5lbD5cbik7XG5cbmV4cG9ydCBkZWZhdWx0IEluZGV4O1xuIl0sIm5hbWVzIjpbIlBhbmVsIiwiVGV4dCIsIkluZGV4IiwiaGVhZGVyIiwibWFyZ2luIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.tsx\n");
+
+/***/ }),
+
+/***/ "@bigcommerce/big-design":
+/*!******************************************!*\
+  !*** external "@bigcommerce/big-design" ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = require("@bigcommerce/big-design");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.tsx"));
+module.exports = __webpack_exports__;
+
+})();
